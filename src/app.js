3276dbf8198dab8dcef4dafce0bf8bcf5f7d8785
SaadIsmaill <saadismail.co@gmail.com>
2025-03-03 07:38:22 +0500

    Responsive Website using HTML Tailwindcss & Javascript by Saad Ismail

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const scroll = ScrollReveal({
+    reset: true,
+    duration: 2600,
+    distance: '60px',
+});
+
+scroll.reveal('.image-transform',{delay: 400, origin: 'top'});
+scroll.reveal('.text-transform',{delay: 300, origin: 'left'});
